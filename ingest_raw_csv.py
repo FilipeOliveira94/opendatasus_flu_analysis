7410ac523d9457b5eafbfe77b53ed3289894a50a
Filipe Oliveira <filipeoliveira94@hotmail.com>
2024-07-31 09:54:57 -0300

    Improve logging

--- ingest_raw_csv.py
+++ ingest_raw_csv.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+from datetime import datetime
+import logging
+import sys
 
-def ingest():
-    
+def ingest(logger):
     def get_request(url):
         s = requests.Session()
         retries = requests.adapters.Retry(total=5, backoff_factor=1)
         s.mount('http://', requests.adapters.HTTPAdapter(max_retries=retries))
-        print(f'Requesting URL: {url}')
+        logger.info(f'Requesting URL: {url}')
         response = s.get(url)
         return response
 
@@ -20,6 +22,7 @@ def ingest():
     soup = BeautifulSoup(response.text, 'html.parser')
     dataset_pages = soup.find_all('a', attrs={'class':'page-link'})
     pagecount = int(dataset_pages[-2].get_text())
+    logger.info(f'Got page count: {pagecount}')
 
     catalog_pages = []
     for page in range(1, pagecount):
@@ -29,6 +32,8 @@ def ingest():
         [catalog_pages.append(domain + link['href']) for link in soup.find_all('a')
         if ('Notificações de Síndrome Gripal' in link.get_text()
             and 'API' not in link.get_text())]
+    if len(catalog_pages) > 0: 
+        logger.info(f'SUCCESS - Got {len(catalog_pages)} catalog pages')
         
     dataset_pages = []
     for page in catalog_pages:
@@ -37,6 +42,8 @@ def ingest():
         [dataset_pages.append(domain + link['href']) for link in soup.find_all('a', {'class':'heading'})
         if ('Dados' in link['title'] 
             and 'Dicionário' not in link['title'])] 
+    if len(dataset_pages) > 0:
+        logger.info(f'SUCCESS - Got {len(dataset_pages)} dataset pages')
         
     csv_links = []
     for page in dataset_pages:
@@ -44,19 +51,37 @@ def ingest():
         soup = BeautifulSoup(response.text, 'html.parser')
         [csv_links.append(link['href']) for link in soup.find_all('a', href=True)
         if 'csv' in link['href']]
+    if len(csv_links) > 0:
+        logger.info(f'SUCCESS - Got {len(csv_links)} csv links')
         
     csv_count = 1
+    error_count = 0
     os.mkdir('raw_data')
     for link in csv_links:
-            print(f'{csv_count} - Reading data from {link}')
-            csv_count += 1
+        logger.info(f'{csv_count} - Reading data from {link}')
+        csv_count += 1
+        try:
             response = get_request(link)
             filepath = f'raw_data/{link.split('/')[5] + '-' + link.split('/')[6][-2:] + '-' + link.split('/')[7]}.csv'
             with open(filepath,'wb') as f:
                 f.write(response.content)
+            logger.info(f'SUCCESS writing file: {filepath}')
+        except:
+            logger.error(f'error writing file: {filepath}')
+            error_count += 1
+    logger.info(f'Success writing {csv_count - error_count} files')
             
 def main():
-    ingest()
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='ingest.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    
+    start_time = datetime.now()
+    logger.info(f'Starting script: {start_time.strftime('%Y-%m-%d %H:%M:%S')}')
+    ingest(logger)
+    end_time = datetime.now()
+    logger.info(f'Ending script: {end_time.strftime('%Y-%m-%d %H:%M:%S')}')
+    logger.info(f'Elapsed time (secs): {round((end_time - start_time).total_seconds(),3)}')
     
 if __name__ == "__main__":
     main()
--- process_data.py
+++ process_data.py
@@ -2,10 +2,13 @@ import pandas as pd
 import os
 import re
 import json
+from datetime import datetime
+import logging
+import sys
 import warnings
 warnings.simplefilter(action='ignore', category=pd.errors.DtypeWarning)
 
-def process():
+def process(logger):
     def save_parquet(df, filepath):
         df = df.astype('str')
         filepath_parquet = 'processed_data/' + filepath.split('/')[-1].split('.')[0] + '.parquet'
@@ -32,40 +35,49 @@ def process():
     skiplist = []
     skipcount = 0
     for i, file in enumerate(os.listdir('raw_data')):
-        print(f'{i+1} - Processing file "{file}"')
+        logger.info(f'{i+1} - Processing file "{file}"')
         filepath = 'raw_data/' + file
         
         try:    
             df = pd.read_csv(filepath, sep=';', header=0)
         except pd.errors.ParserError:
-            print(f'{i+1} - Error parsing file "{file}" - Attempting to clean data')
+            logger.info(f'{i+1} - Error parsing file "{file}" - Attempting to clean data')
             try:
                 df = clean_bad_csv(filepath)
             except Exception as exception:
-                print(f'{i+1} - SKIPPING - Error parsing: {file}')
+                logger.error(f'{i+1} - SKIPPING - Error parsing: {file}')
                 skipcount = log_error(filepath, 'parse_clean', exception, skipcount)
                 continue
         except Exception as exception:
-            print(f'{i+1} - SKIPPING - Error parsing: {file}')
+            logger.error(f'{i+1} - SKIPPING - Error parsing: {file}')
             skipcount = log_error(filepath, 'parse_general', exception, skipcount)
             continue
         
         try:
             save_parquet(df, filepath)
         except Exception as exception:
-            print(f'{i+1} - SKIPPING - Error writing to .parquet file: {file}')
+            logger.error(f'{i+1} - SKIPPING - Error writing to .parquet file: {file}')
             skipcount = log_error(filepath, 'write_parquet', exception, skipcount)
             continue
         
-        print(f'{i+1} - Success saving file "{file}"')
+        logger.info(f'{i+1} - Success saving file "{file}"')
         
-    print(f'Success writing {(i+1) - skipcount} files. Skipped count: {skipcount}')
+    logger.info(f'Success writing {(i+1) - skipcount} files. Skipped count: {skipcount}')
     
     with(open('skiplist.json', 'w')) as f:
         json.dump(skiplist, f, indent=4)
 
 def main():
-    process()
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='ingest.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    
+    start_time = datetime.now()
+    logger.info(f'Starting script: {start_time.strftime('%Y-%m-%d %H:%M:%S')}')
+    process(logger)
+    end_time = datetime.now()
+    logger.info(f'Ending script: {end_time.strftime('%Y-%m-%d %H:%M:%S')}')
+    logger.info(f'Elapsed time (secs): {round((end_time - start_time).total_seconds(),3)}')
     
 if __name__ == "__main__":
     main()
